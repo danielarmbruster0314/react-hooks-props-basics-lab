2c94507ef0211464570555e21e15205e49ebf5bb
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-14 11:02:49 -0600

    commit tree 9b265a8d5092d43c1dc8f55732c07ebfd28bbdc7

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,41 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import Links from "./Links";
+// function About(props) {
+  
+  
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       <p>{props.bio && props.bio.lenght > 1 ? props.bio : null}</p>
+//       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+//       <Links github = {props.links.github} linkedin = {props.links.linkedin} />
+//     </div>
+//   );
+// }
+
+// export default About;
